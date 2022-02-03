cc73edf092a1520d7421ef51795a6eb1b0b5fd89
Sonya <labe.sonia@gmail.com>
2022-02-03 16:54:55 +0100

    I set up backend and added routes.

---
+++ conf.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_SCHEMA } = process.env;
+
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_SCHEMA,
+});
+
+module.exports = {
+  db,
+  backPort: PORT,
+};
---
+++ routes/comments.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { db } = require('../conf');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const [comments] = await db.query(
+      `SELECT id, message, nameAuthor, date, idPages FROM comments`
+    );
+    if (comments.length) {
+      res.status(200).json(comments);
+    } else {
+      res.status(404).send('Comments not found');
+    }
+  } catch (err) {
+    res.status(500).send('Error retrieving the comments');
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [comments] = await db.query(
+      `SELECT id, message, nameAuthor, date, idPages FROM comments WHERE id = ?`,
+      [id]
+    );
+    if (comments.length) {
+      res.status(200).json(comments);
+    } else {
+      res.status(404).send('Comment not found');
+    }
+  } catch (err) {
+    res.status(500).send('Error retrieving the comment');
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { message, nameAuthor, idPages, idParent } = req.body;
+    await db.query(
+      `INSERT INTO comments (message, nameAuthor, idPages, idParent, date) VALUES (?, ?, ?, ?, NOW())`,
+      [message, nameAuthor, idPages, idParent]
+    );
+    res.status(201).send('Comment created');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error creating the comment');
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await db.query(`DELETE FROM comments WHERE id = ?`, [id]);
+    res.status(200).send('Comment succesfully deleted');
+  } catch (err) {
+    res.status(500).send('Error deleting the comment');
+  }
+});
+
+module.exports = router;
